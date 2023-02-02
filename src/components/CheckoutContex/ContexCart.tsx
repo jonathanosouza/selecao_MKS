dc708c38a09e1dfc22e2b6ddc88e0b7594128f39
Jonathan Souza <jonathanosouza@gmail.com>
2023-02-01 21:23:47 -0300

    Change One

--- src/components/CheckoutContex/ContexCart.tsx
+++ src/components/CheckoutContex/ContexCart.tsx
@@ -39,7 +39,7 @@ interface ProductsProps {
   handleIncrementToCart: (id: number) => void
   handleDeleteToCart: (id: number) => void
   openModal: () => void
-  setLoading: () => void
+  setLoading: boolean
   closeModal: () => void
   loading: boolean
 
